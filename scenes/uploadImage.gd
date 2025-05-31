8941f521feaca58d718e81530e14a2c2dc53881d
mmaxwellgaming777 <maxwellgaming777@gmail.com>
2025-05-31 14:58:38 -0500

    Added a generate button that transfers the texturerects texture to a png and saves it on chosen file system directory. Having issue with choosing a file now with the native file dialog.

--- scenes/uploadImage.gd
+++ scenes/uploadImage.gd
@@ -1,20 +1,33 @@
 extends Node2D
 
-@onready var file_dialog = $FileDialog
+@onready var upload_file_dialog = $FileDialog
 @onready var og_texture_rect = $OgTextureRect
 @onready var cloned_texture_rect = $ClonedTextureRect
 @onready var color_change_module = $ColorChangeModule
 @onready var nft_container = $NFTContainer
+@onready var nft_collection_name = $NFTCollectionName
+@onready var save_dialog = $SaveDialog
 
 var NFTLayer = 0
 var NFTCount = 1
 
 func _ready():
-	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
-	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
-	file_dialog.filters = ["*.png ; PNG Images", "*.jpg ; JPEG Images", "*.jpeg ; JPEG Images"]
-	file_dialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
-	file_dialog.connect("file_selected", _on_file_selected)
+	upload_file_dialog.file_mode = FileDialog.ACCESS_FILESYSTEM
+	upload_file_dialog.access = FileDialog.FILE_MODE_OPEN_FILE
+	upload_file_dialog.use_native_dialog = true
+	upload_file_dialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
+	#upload_file_dialog.filters = ["*.png ; PNG Images", "*.jpg ; JPEG Images", "*.jpeg ; JPEG Images"]
+	upload_file_dialog.filters = ["*.png ; PNG Images"]
+	upload_file_dialog.connect("file_selected", _on_file_selected)
+	
+	save_dialog.access = FileDialog.FILE_MODE_OPEN_DIR
+	save_dialog.file_mode = FileDialog.ACCESS_FILESYSTEM
+	save_dialog.use_native_dialog = true
+	save_dialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
+	save_dialog.title = "Choose a folder to save files into"
+	#save_dialog.filters = ["*.* ; Any File"]
+	#save_dialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
+	save_dialog.connect("file_selected", _on_save_path_chosen)
 	
 
 func _on_file_selected(path):
@@ -32,9 +45,9 @@ func _on_file_selected(path):
 
 
 func _on_upload_button_pressed() -> void:
-	file_dialog.popup_centered()
-
-func _on_clone_button_pressed() -> void:
+	upload_file_dialog.popup_centered()
+	
+func _on_save_path_chosen(path: String) -> void:
 	var image = Image.new()
 	var error = image.load("user://NFTLayer%d.png" % NFTLayer)
 	var textureRect = TextureRect.new()
@@ -50,8 +63,25 @@ func _on_clone_button_pressed() -> void:
 	color_change_module.changeColor(textureRect, NFTLayer)
 	textureRect.texture = shrink_texture(textureRect.texture, 0.5)
 	
+	if textureRect.texture is Texture2D:
+		var imageToSave: Image = textureRect.texture.get_image()
+		if image:
+			var error1 = imageToSave.save_png(path + nft_collection_name.text + ".png")
+			if error == OK:
+				print("Image saved to: ", path)
+			else:
+				push_error("Failed to save image. Error code: %d" % error)
+		else:
+			push_error("Couldn't get image from texture.")
+	else:
+		push_error("Texture is not a Texture2D.")
+	
 func shrink_texture(texture: Texture2D, scale: float) -> Texture2D:
 	var img := texture.get_image()
 	var new_size := img.get_size() * scale
 	img.resize(new_size.x, new_size.y, Image.INTERPOLATE_LANCZOS)
 	return ImageTexture.create_from_image(img)
+
+
+func _on_clone_button_pressed() -> void:
+	save_dialog.popup_centered()
